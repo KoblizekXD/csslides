b9b62d70892bf813ee9544701b4d40f0402557b8
KoblizekXD <janprokupek@centrum.cz>
2025-01-24 10:19:41 +0100

    Initialize the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main />;
+  return (
+    <main className="h-screen flex flex-col w-full">
+      <nav className="border-b border-muted blur">
+        <div>
+          csslides
+        </div>
+      </nav>
+      <div>
+
+      </div>
+    </main>
+  );
 }
